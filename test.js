0a96a40e0e5c0592ea05e1ebc1dadf190201958a
lihuafeng <lihuafeng@gf.com.cn>
2016-07-04 11:13:41 +0800

    change activiti, add authorization basic

--- test.js
+++ test.js
@@ -3,8 +3,45 @@ var activiti = require('./lib/activiti');
 
 var opts = {
     host: '10.2.122.16', 
-    port: 8082,
-    username: '',
-    password: ''
+    port: 8082
+    
 };
 var client = new activiti(opts);
+
+client.startProcessByKey('create_new_portal_user')
+    .then(function(result){
+        console.log(JSON.stringify(result));
+    })
+    .fail(console.log)
+    .done();
+
+// client.runtimeTasks('5018')
+//     .then(function(result){
+//         console.log(JSON.stringify(result));
+//     })
+//     .fail(console.log)
+//     .done();
+
+// client.completeTask('5030', {mobile: '18680560728', user: 'jack'})
+//     .then(function(result){
+//         console.log(JSON.stringify(result));
+//     })
+//     .fail(console.log)
+//     .done();
+
+
+// client.getHistoricProcessInstance('5018')
+//     .then(function(result){
+//         console.log(JSON.stringify(result));
+//     })
+//     .fail(console.log)
+//     .done();
+
+// client.isProcessCompleted('5018')
+//     .then(function(result){
+//         console.log(JSON.stringify(result));
+//     })
+//     .fail(console.log)
+//     .done();
+
+    
